f913d89fcd2c7d2710c22bf3208e7e748c09dd9f
Hoang Doan Duy <hoangdoan1105206@gmail.com>
2026-08-03 15:10:59 +0700

    update khung và State của Recipe Detail Page

--- lib/pages/recipe_detail_page.dart
+++ lib/pages/recipe_detail_page.dart
@@ -3,6 +3,7 @@ import '../models/recipe.dart';
 
 class RecipeDetailPage extends StatefulWidget {
   final Recipe recipe;
+
   const RecipeDetailPage({super.key, required this.recipe});
 
   @override
@@ -12,9 +13,35 @@ class RecipeDetailPage extends StatefulWidget {
 class _RecipeDetailPageState extends State<RecipeDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Recipe Detail Page',
-      style: Theme.of(context).textTheme.headlineMedium,
+    return Scaffold(
+      backgroundColor: const Color(0xffF5FAF6),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            //Top Image
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 30),
+
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: [
+                    //Recipe Info
+                    const SizedBox(height: 24),
+
+                    //_buildIngredients(),
+                    const SizedBox(height: 24),
+
+                    //Step of Cooking
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
