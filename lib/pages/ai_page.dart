37541e013cbabae4a51df7bf9421dbad7b00cea2
Hoang Doan Duy <hoangdoan1105206@gmail.com>
2026-08-03 11:09:05 +0700

    tạo khung AiPage

--- lib/pages/ai_page.dart
+++ lib/pages/ai_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class AiPage extends StatelessWidget {
+class AiPage extends StatefulWidget {
   const AiPage({super.key});
 
   @override
+  State<AiPage> createState() => _AiPageState();
+}
+
+class _AiPageState extends State<AiPage> {
+  @override
   Widget build(BuildContext context) {
-    return Text('AI Page', style: Theme.of(context).textTheme.headlineMedium);
+    return Scaffold(
+      backgroundColor: const Color(0xffF5FAF6),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            //_buildHeader(),
+
+            //_buildSearchBar(),
+
+            //_buildCategoryFilter(),
+          ],
+        ),
+      ),
+    );
   }
 }
