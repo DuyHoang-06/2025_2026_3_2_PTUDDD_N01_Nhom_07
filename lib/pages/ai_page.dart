6991bf97e9226e85e35f6aa73c89f52db0823a90
Hoang Doan Duy <hoangdoan1105206@gmail.com>
2026-08-03 11:11:46 +0700

    build phần header

--- lib/pages/ai_page.dart
+++ lib/pages/ai_page.dart
@@ -16,12 +16,31 @@ class _AiPageState extends State<AiPage> {
       body: SafeArea(
         child: Column(
           children: [
-            //_buildHeader(),
+            _buildHeader(),
 
             //_buildSearchBar(),
 
             //_buildCategoryFilter(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 16, 16, 10),
+
+      child: Align(
+        alignment: Alignment.centerLeft,
+
+        child: Text(
+          'Thư Viện Công Thức',
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff1D3022),
+          ),
         ),
       ),
     );
